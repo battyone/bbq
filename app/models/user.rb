ed7a89a07eeb8f128d03a5ca8dc9c88c40b96555
Edem <keltikan@gmail.com>
2019-02-24 20:24:56 +0300

    Add user to events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
