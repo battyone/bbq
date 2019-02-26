2e9eb4a7e8450cc902663b717c0a4702034c8e78
Edem <keltikan@gmail.com>
2019-02-26 16:00:01 +0300

    Views edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,4 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
-
-  before_validation :set_name, on: :create
-
-  private
-
-  def set_name
-    self.name = Faker::DcComics.hero if self.name.blank?
-  end
 end
