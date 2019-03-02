618fdb2fe0c0efa2eddb97440b3659279d9c3468
Edem <keltikan@gmail.com>
2019-03-03 01:11:35 +0300

    Model Comment added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
