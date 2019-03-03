827d5a308093915a5c122e64c2d62c22f8ca7626
Edem <keltikan@gmail.com>
2019-03-03 21:36:46 +0300

    Event user can't subscribe

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,7 +6,9 @@ class SubscriptionsController < ApplicationController
     @new_subscription = @event.subscriptions.build(subscription_params)
     @new_subscription.user = current_user
 
-    if @new_subscription.save
+    if current_user == @event.user
+      redirect_to @event, alert: I18n.t('controllers.subscriptions.error')
+    elsif @new_subscription.save
       redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
     else
       render 'events/show', alert: I18n.t('controllers.subscriptions.error')
