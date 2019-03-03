d399aecc3e2f0498cd8ffcbcef2d8b2d1a368185
Edem <keltikan@gmail.com>
2019-03-03 20:35:51 +0300

    Link anonymous subscriptions to new user with the same email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
+
+  after_commit :link_subscriptions, on: :create
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
+  end
 end
