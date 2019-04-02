631429ea63f9ca3036dcef5f8ef7af647b859ffe
Edem <keltikan@gmail.com>
2019-04-02 23:13:43 +0300

    RSpec for Event policy with cookies context

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -5,25 +5,30 @@ RSpec.describe EventPolicy do
 
   let(:user) { FactoryBot.create(:user) }
   let(:other_user) { FactoryBot.create(:user) }
+
+  let(:user_context) { UserContext.new(user, nil) }
+  let(:other_user_context) { UserContext.new(other_user, nil) }
+  let(:not_user_context) { UserContext.new(nil, '1234') }
+
   let(:event) { FactoryBot.create(:event, user: user) }
   let(:event_with_pin) { FactoryBot.create(:event, user: user, pincode: '1234') }
 
   context 'User try to edit, update or destroy event' do
     context 'when user is owner' do
       permissions :edit?, :update?, :destroy? do
-        it { is_expected.to permit(user, event) }
+        it { is_expected.to permit(user_context, event) }
       end
     end
 
     context 'when user is not owner' do
       permissions :edit?, :update?, :destroy? do
-        it { is_expected.not_to permit(other_user, event) }
+        it { is_expected.not_to permit(other_user_context, event) }
       end
     end
 
     context 'when user is not authentificated' do
       permissions :edit?, :update?, :destroy? do
-        it { is_expected.not_to permit(nil, event) }
+        it { is_expected.not_to permit(not_user_context, event) }
       end
     end
   end
@@ -31,32 +36,28 @@ RSpec.describe EventPolicy do
   context 'User try to look event' do
     context 'when pincode is absent' do
       permissions :show? do
-        it { is_expected.to permit(nil, event) }
+        it { is_expected.to permit(other_user_context, event) }
       end
     end
 
     context 'when pincode is present' do
       context 'and user is owner' do
         permissions :show? do
-          it { is_expected.to permit(user, event_with_pin) }
+          it { is_expected.to permit(user_context, event_with_pin) }
         end
       end
 
       context 'and user has valid cookies' do
+        before { allow(event_with_pin).to receive(:pincode_valid?).with('1234') }
+
         permissions :show? do
-          it do
-            allow(cookies).to receive(:permanent["events_#{event_with_pin.id}_pincode"]).and_return '1234'
-            is_expected.to permit(nil, event_with_pin)
-          end
+          it { is_expected.to permit(not_user_context, event_with_pin) }
         end
       end
 
       context 'and user has not valid cookies' do
         permissions :show? do
-          it do
-            allow(cookies).to receive(:permanent["events_#{event_with_pin.id}_pincode"]).and_return nil
-            is_expected.not_to permit(nil, event_with_pin)
-          end
+          it { is_expected.not_to permit(other_user_context, event_with_pin) }
         end
       end
     end
