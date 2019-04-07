29972378934506949915e0d4f11abf742b1cd1c5
Edem <keltikan@gmail.com>
2019-04-07 20:54:31 +0300

    Routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'registrations',
+    registrations: 'devise/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
